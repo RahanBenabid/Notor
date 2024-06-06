fe2cf2effd356d1619ee5d30c61e717be33f1080
RahanBenabid <rahannadime@gmail.com>
2024-06-06 22:35:15 +0100

    Initial Commit

---
+++ Notor/NotorApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotorApp.swift
+//  Notor
+//
+//  Created by Rahan Benabid on 6/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct NotorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
